4738dbcf9ec91c574b9511c5a9a1aadb36ec40e1
sussymon <84589247+sussymon@users.noreply.github.com>
2022-06-24 15:10:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="/css/style.css">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE-edge">
+<meta name="viewport" content="width-device-witdth, initial-scale-1.0">
+<title>Minecraft Sex Mod</title>
+<h1>Minecraft Sex Mod</h1>
+</head>
+<body>
+    <p>
+        Minecraft Jenny Sex Mod
+    </p>
+    <footer>Site by nerd</footer>
+</body>
+</html>
